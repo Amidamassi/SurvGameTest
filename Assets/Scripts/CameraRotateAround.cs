a229f39e2969689d932517d86aad4549bc9a075a
KcinKcip <fedor_pushko@mail.ru>
2019-03-10 21:17:04 +0300

    0.06 omega
    0.06 omega version

--- Assets/Scripts/CameraRotateAround.cs
+++ Assets/Scripts/CameraRotateAround.cs
@@ -6,48 +6,35 @@ public class CameraRotateAround : MonoBehaviour
 {
 
     public Transform target;
-    Vector3 offset;
-    public float sensitivity = 3; // чувствительность мышки
-    //public float limit = 80; // ограничение вращения по Y
-    public float zoom = 0.25f; // чувствительность при увеличении, колесиком мышки
-    public float zoomMax = 10; // макс. увеличение
-    public float zoomMin = 3; // мин. увеличение
-    private float X, Y;
-    float distanse;
+    private float sensitivity = 3; // чувствительность мышки
+    private float xCoordinate;
+    private float yCoordinate;
+    private float xMinAngle = -70;
+    private float xMaxAngle = -15;
     float radius;
-    Vector3 hui;
+
+    Vector3 cameraRelativeToCharacter;
 
     void Start()
     {
-        offset = transform.position - target.transform.position;
-        //limit = Mathf.Abs(limit);
-        //if (limit > 90) limit = 90;
-        //offset = new Vector3(offset.x, offset.y, -Mathf.Abs(zoomMax)/2);
-        //transform.position = target.position + offset;
-        radius = Vector3.Magnitude(new Vector3(transform.position.x-target.position.x, 0, transform.position.z- target.position.z));
+        xCoordinate = -transform.eulerAngles.x;
+        yCoordinate = transform.eulerAngles.y;
+        radius = Vector3.Magnitude(new Vector3(transform.position.x-target.position.x, transform.position.y - target.position.y, transform.position.z- target.position.z));
+        transform.localEulerAngles = new Vector3(-xCoordinate, yCoordinate, 0);
+        cameraRelativeToCharacter = -target.transform.InverseTransformVector(transform.forward).normalized * radius;
+        transform.position = target.position + cameraRelativeToCharacter;
     }
 
     void Update()
     {
-        //if (Input.GetAxis("Mouse ScrollWheel") > 0) offset.z += zoom;
-        //else if (Input.GetAxis("Mouse ScrollWheel") < 0) offset.z -= zoom;
-        //offset.z = Mathf.Clamp(offset.z, -Mathf.Abs(zoomMax), -Mathf.Abs(zoomMin));
-        if (Input.GetMouseButton(0)) {         
-            
-            //X = transform.localEulerAngles.x + Input.GetAxis("Mouse X") * sensitivity;
-            Y += Input.GetAxis("Mouse X") * sensitivity;
-            transform.localEulerAngles = new Vector3(45, -Y, 0);
-            distanse = Vector3.Distance(target.position, transform.position);
-            hui = -target.transform.InverseTransformVector(transform.forward);
-            hui.y = 0;
-            hui =hui.normalized*radius;
-            transform.position = new Vector3(target.position.x + hui.x, transform.position.y, hui.z+target.position.z);
-            //transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
-            //transform.Rotate(new Vector3(0, Y, 0));
-            //transform.di
-            //transform.localEulerAngles = new Vector3(45, -Y, 0);
-            //transform.position = transform.position + Vector3.back * distanse;
-            //transform.position = transform.localRotation * offset - target.transform.position;
+        if (Input.GetMouseButton(0)) {
+            xCoordinate += Input.GetAxis("Mouse Y") * sensitivity;
+            xCoordinate = Mathf.Clamp(xCoordinate, xMinAngle, xMaxAngle);
+            yCoordinate += Input.GetAxis("Mouse X") * sensitivity;
+            transform.localEulerAngles = new Vector3(-xCoordinate, yCoordinate, 0);
+            cameraRelativeToCharacter = -target.transform.InverseTransformVector(transform.forward);
+            cameraRelativeToCharacter = cameraRelativeToCharacter.normalized*radius;
+            transform.position = target.position + cameraRelativeToCharacter;
         }
 
     }
