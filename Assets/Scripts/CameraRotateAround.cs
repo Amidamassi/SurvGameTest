016ad5e4f3886c9025a5fe7f4f81737fdb419741
KcinKcip <fedor_pushko@mail.ru>
2019-03-10 19:07:57 +0300

    0.05 omega
    0.05 omega version

---
+++ Assets/Scripts/CameraRotateAround.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotateAround : MonoBehaviour
+{
+
+    public Transform target;
+    Vector3 offset;
+    public float sensitivity = 3; // чувствительность мышки
+    //public float limit = 80; // ограничение вращения по Y
+    public float zoom = 0.25f; // чувствительность при увеличении, колесиком мышки
+    public float zoomMax = 10; // макс. увеличение
+    public float zoomMin = 3; // мин. увеличение
+    private float X, Y;
+    float distanse;
+    float radius;
+    Vector3 hui;
+
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+        //limit = Mathf.Abs(limit);
+        //if (limit > 90) limit = 90;
+        //offset = new Vector3(offset.x, offset.y, -Mathf.Abs(zoomMax)/2);
+        //transform.position = target.position + offset;
+        radius = Vector3.Magnitude(new Vector3(transform.position.x-target.position.x, 0, transform.position.z- target.position.z));
+    }
+
+    void Update()
+    {
+        //if (Input.GetAxis("Mouse ScrollWheel") > 0) offset.z += zoom;
+        //else if (Input.GetAxis("Mouse ScrollWheel") < 0) offset.z -= zoom;
+        //offset.z = Mathf.Clamp(offset.z, -Mathf.Abs(zoomMax), -Mathf.Abs(zoomMin));
+        if (Input.GetMouseButton(0)) {         
+            
+            //X = transform.localEulerAngles.x + Input.GetAxis("Mouse X") * sensitivity;
+            Y += Input.GetAxis("Mouse X") * sensitivity;
+            transform.localEulerAngles = new Vector3(45, -Y, 0);
+            distanse = Vector3.Distance(target.position, transform.position);
+            hui = -target.transform.InverseTransformVector(transform.forward);
+            hui.y = 0;
+            hui =hui.normalized*radius;
+            transform.position = new Vector3(target.position.x + hui.x, transform.position.y, hui.z+target.position.z);
+            //transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+            //transform.Rotate(new Vector3(0, Y, 0));
+            //transform.di
+            //transform.localEulerAngles = new Vector3(45, -Y, 0);
+            //transform.position = transform.position + Vector3.back * distanse;
+            //transform.position = transform.localRotation * offset - target.transform.position;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerCraft.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCraft : MonoBehaviour
+{
+    bool tryToCraft = false;
+    bool showCraft = false;
+    GameObject campFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        campFire = Resources.Load<GameObject>("Craft/CampFire");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)) { tryToCraft = true; }
+        if (tryToCraft) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (!showCraft) {
+                    campFire.transform.position = hit.point;
+                    campFire.GetComponent<BoxCollider>().enabled = false;
+                    campFire = Instantiate(campFire);
+                    showCraft = true;
+                }
+                if (showCraft) {
+                    campFire.transform.position = hit.point;
+                    if (Input.GetKeyDown(KeyCode.Mouse0)) {
+                        tryToCraft = false;
+                        showCraft = false;
+                        campFire.GetComponent<BoxCollider>().enabled = true;
+                        campFire = Resources.Load<GameObject>("Craft/CampFire");
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.Escape)) {
+                    tryToCraft = false;
+                }
+            }
+        }
+            
+        
+    }
+}
